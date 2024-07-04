ae02b6fd62b2253e531ed3ed000a43fdf7c6bafd
efigb <elisabet.figuerola@irbbarcelona.org>
2024-07-04 12:17:51 +0200

    added script to calculate mutation reduction and corrected figure titles and legends in damage notebook

---
+++ variant_analysis/somatic/consensus_mut/reduction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ask user to introduce initial number of mutations and final number after quality filter
+echo "Introduce the initial number of mutations:"
+read initial
+echo "Introduce the final number of mutations after quality filtering:"
+read final
+
+# Calculate the difference in the number of variants
+reduction_abs=$((initial - final))
+
+# Calculate reduction percent
+percent_reduction=$(echo "scale=4; ($reduction_abs / $initial) * 100" | bc)
+
+# Show the result
+echo "This is the percent of reduction after quality filter: $percent_reduction%"
+echo "This is the reduction in variants after selecting the consensus set (calculated from initial number): $difference"
+
